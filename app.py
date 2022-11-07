9db98a4d23eaf6d43ec352455e10d3fbea5d2485
Varunvaibav <varunvaibavsritharan@gmail.com>
2022-11-08 00:41:48 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,198 @@
+import streamlit as st
+import argparse
+import io
+import os
+from PIL import Image
+import cv2
+import numpy as np
+import torch
+import time
+import re
+from io import BytesIO
+import easyocr
+from functions import *
+
+st.set_page_config(
+    page_title="Auto NPR",
+    page_icon="✨",
+    layout="centered",
+    initial_sidebar_state="expanded",
+)
+
+
+top_image = Image.open('static/banner_top.png')
+bottom_image = Image.open('static/banner_bottom.png')
+main_image = Image.open('static/main_banner.png')
+
+upload_path = "uploads/"
+download_path = "downloads/"
+# initiating the model
+model =  torch.hub.load('./yolov7-main', 'custom', source ='local', path_or_model='best.pt',force_reload=True)
+classes = model.names
+
+st.image(main_image,use_column_width='auto')
+st.title(' Automatic Number Plate Recognition 🚘🚙')
+st.sidebar.image(top_image,use_column_width='auto')
+st.sidebar.header('Input 🛠')
+selected_type = st.sidebar.selectbox('Please select an activity type 🚀', ["Upload Image","Upload Video", "Live Video Feed"])
+st.sidebar.image(bottom_image,use_column_width='auto')
+
+# Code for images as input
+if selected_type == "Upload Image":
+    st.info('✨ Supports all popular image formats 📷 - PNG, JPG, BMP 😉')
+    uploaded_file = st.file_uploader("Upload Image of car's number plate 🚓", type=["png","jpg","bmp","jpeg"])
+
+    if uploaded_file is not None:
+        with open(os.path.join(upload_path,uploaded_file.name),"wb") as f:
+            f.write((uploaded_file).getbuffer())
+        with st.spinner(f"Working... 💫"):
+            uploaded_image = os.path.abspath(os.path.join(upload_path,uploaded_file.name))
+            downloaded_image = os.path.abspath(os.path.join(download_path,str("output_"+uploaded_file.name)))
+
+            with open(uploaded_image,'rb') as imge:
+                img_bytes = imge.read()
+            
+            img = Image.open(io.BytesIO(img_bytes))
+
+            frame =cv2.imread(uploaded_image)
+
+            frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+        
+            result = detectx(frame, model = model) # Detects the position of the license plate
+
+            frame = cv2.cvtColor(frame,cv2.COLOR_RGB2BGR)
+
+            frame = plot_boxes((result[0],result[1]), frame,classes = classes)
+            frame = cv2.cvtColor(frame,cv2.COLOR_RGB2BGR)
+            img_base64 = Image.fromarray(frame)
+
+            img_base64.save(downloaded_image, format="JPEG")
+            
+            final_image = Image.open(downloaded_image)
+            print("Opening ",final_image)
+            st.markdown("---")
+            st.image(final_image, caption='This is how your final image looks like 😉')
+            
+            with open(downloaded_image, "rb") as file:
+                if uploaded_file.name.endswith('.jpg') or uploaded_file.name.endswith('.JPG'):
+                    if st.download_button(
+                                            label="Download Output Image 📷",
+                                            data=file,
+                                            file_name=str("output_"+uploaded_file.name),
+                                            mime='image/jpg'
+                                            ):
+                        download_success()
+                if uploaded_file.name.endswith('.jpeg') or uploaded_file.name.endswith('.JPEG'):
+                    if st.download_button(
+                                            label="Download Output Image 📷",
+                                            data=file,
+                                            file_name=str("output_"+uploaded_file.name),
+                                            mime='image/jpeg'
+                                            ):
+                        download_success()
+
+                if uploaded_file.name.endswith('.png') or uploaded_file.name.endswith('.PNG'):
+                    if st.download_button(
+                                            label="Download Output Image 📷",
+                                            data=file,
+                                            file_name=str("output_"+uploaded_file.name),
+                                            mime='image/png'
+                                            ):
+                        download_success()
+
+                if uploaded_file.name.endswith('.bmp') or uploaded_file.name.endswith('.BMP'):
+                    if st.download_button(
+                                            label="Download Output Image 📷",
+                                            data=file,
+                                            file_name=str("output_"+uploaded_file.name),
+                                            mime='image/bmp'
+                                            ):
+                        download_success()
+    else:
+        st.warning('⚠ Please upload your Image 😯')
+
+# Code for Live Feed as input
+elif selected_type == "Live Video Feed":
+    st.info('✨ The Live Feed from Web-Camera will take some time to load up 🎦')
+    live_feed = st.checkbox('Start Web-Camera ✅')
+    FRAME_WINDOW = st.image([])
+    cap = cv2.VideoCapture(0)
+
+    if live_feed:
+        while(cap.isOpened()):
+            success, frame = cap.read()
+            if success == True:
+                ret,buffer=cv2.imencode('.jpg',frame)
+
+                frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+        
+                result = detectx(frame, model = model)
+
+                frame = cv2.cvtColor(frame,cv2.COLOR_RGB2BGR)
+
+                frame = plot_boxes((result[0],result[1]), frame,classes = classes)
+
+                frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+
+            else:
+                break
+            frame = cv2.imencode('.jpg', frame)[1].tobytes()
+            FRAME_WINDOW.image(frame)
+    else:
+        cap.release()
+        cv2.destroyAllWindows()
+        st.warning('⚠ The Web-Camera is currently disabled. 😯')
+
+# Code for Video as input
+elif selected_type == "Upload Video":
+
+    st.info('✨ Upload a MP4 file ')
+    uploaded_file = st.file_uploader("Upload video of car's number plate 🚓", type=["mp4"])
+    FRAME_WINDOW = st.image([])
+
+    if uploaded_file is not None:
+        with open(os.path.join(upload_path,uploaded_file.name),"wb") as f:
+            f.write((uploaded_file).getbuffer())
+        with st.spinner(f"Working... 💫"):
+            uploaded_video = os.path.abspath(os.path.join(upload_path,uploaded_file.name))
+            downloaded_video = os.path.abspath(os.path.join(download_path,str("output_"+uploaded_file.name)))
+        
+        cap = cv2.VideoCapture(uploaded_video)
+        width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        fps = int(cap.get(cv2.CAP_PROP_FPS))
+        codec = cv2.VideoWriter_fourcc(*'mp4v') ##(*'XVID')
+        out = cv2.VideoWriter(downloaded_video, codec, fps, (width, height))
+        
+        while(cap.isOpened()):
+            success, frame = cap.read()
+            if success == True:
+                ret,buffer=cv2.imencode('.jpg',frame)
+                frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+        
+                result = detectx(frame, model = model)
+
+                frame = cv2.cvtColor(frame,cv2.COLOR_RGB2BGR)
+
+                frame = plot_boxes((result[0],result[1]), frame,classes = classes)
+                out.write(frame)
+ 
+            else:
+                break
+
+            frame = cv2.imencode('.jpg', frame)[1].tobytes()
+            FRAME_WINDOW.image(frame)
+        out.release()
+        cap.release()
+        with open(downloaded_video, "rb") as file:
+            if uploaded_file.name.endswith('.mp4') or uploaded_file.name.endswith('.mp4'):
+                if st.download_button(
+                                        label="Download Output video 📷",
+                                        data=file,
+                                        file_name=str("output_"+uploaded_file.name),
+                                        mime='video/mp4'
+                                        ):
+                    download_success()
+        
+        cv2.destroyAllWindows()
+        
---
+++ functions.py
@@ -0,0 +1,89 @@
+import streamlit as st
+import cv2
+import numpy as np
+from io import BytesIO
+import easyocr
+
+
+EASY_OCR = easyocr.Reader(['en'])
+OCR_TH = 0.2
+
+#To detect the coordintes of the image
+@st.cache(persist=True,allow_output_mutation=True,show_spinner=False,suppress_st_warning=True)
+def detectx (frame, model):
+    frame = [frame]
+    print(f"[INFO] Detecting. . . ")
+    results = model(frame)
+
+    labels, cordinates = results.xyxyn[0][:, -1], results.xyxyn[0][:, :-1]
+
+    return labels, cordinates,results
+
+# Takes the coordinates as input and outputs the blocks of the liscense plate detected
+@st.cache(persist=True,allow_output_mutation=True,show_spinner=False,suppress_st_warning=True)
+def plot_boxes(results, frame,classes):
+
+    labels, cord = results
+    n = len(labels)
+    x_shape, y_shape = frame.shape[1], frame.shape[0]
+
+    print(f"[INFO] Total {n} detections. . . ")
+    print(f"[INFO] Looping through all detections. . . ")
+
+
+    ### looping through the detections
+    for i in range(n):
+        row = cord[i]
+        if row[4] >= 0.55: 
+            print(f"[INFO] Extracting BBox coordinates. . . ")
+            x1, y1, x2, y2 = int(row[0]*x_shape), int(row[1]*y_shape), int(row[2]*x_shape), int(row[3]*y_shape) ## BBOx coordniates
+            text_d = classes[int(labels[i])]
+
+            coords = [x1,y1,x2,y2]
+
+            plate_num = recognize_plate_easyocr(img = frame, coords= coords, reader= EASY_OCR, region_threshold= OCR_TH)
+
+
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2) ## BBox
+            cv2.rectangle(frame, (x1, y1-20), (x2, y1), (0, 255,0), -1) ## for text label background
+            cv2.putText(frame, f"{plate_num}", (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(255,255,255), 2)
+
+
+    return frame
+
+# Converts the detected images into text
+@st.cache(persist=True,allow_output_mutation=True,show_spinner=False,suppress_st_warning=True)
+def recognize_plate_easyocr(img, coords,reader,region_threshold):
+    xmin, ymin, xmax, ymax = coords
+    nplate = img[int(ymin):int(ymax), int(xmin):int(xmax)]
+
+    ocr_result = reader.readtext(nplate)
+
+
+
+    text = filter_text(region=nplate, ocr_result=ocr_result, region_threshold= region_threshold)
+
+    if len(text) ==1:
+        text = text[0].upper()
+    return text
+
+# Used to filter out unnecessary information on the license plate
+@st.cache(persist=True,allow_output_mutation=True,show_spinner=False,suppress_st_warning=True)
+def filter_text(region, ocr_result, region_threshold):
+    rectangle_size = region.shape[0]*region.shape[1]
+    
+    plate = [] 
+    print(ocr_result)
+    for result in ocr_result:
+        length = np.sum(np.subtract(result[0][1], result[0][0]))
+        height = np.sum(np.subtract(result[0][2], result[0][1]))
+        
+        if length*height / rectangle_size > region_threshold:
+            plate.append(result[1])
+    return plate
+
+
+@st.cache(persist=True,allow_output_mutation=True,show_spinner=False,suppress_st_warning=True)
+def download_success():
+    st.balloons()
+    st.success('✅ Download Successful !!')
